cb14d86dfbeda7b3eef290b4d1b8879528dead2e
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2020-12-16 23:29:01 -0500

    Ajustando App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edita <code>src/App.js</code> para hacer algún ajuste y seguir probando
+            Edita <code>src/App.js</code> para hacer algún ajuste y seguir probando para validar
           </p>
           <a
             className="App-link"
